07854d936d90fe84c084e8c8780d37f06a519d22
Taiyo Scanlon-Kimura <taiyo.scanlon-kimura@fulbrightmail.org>
2023-04-26 19:31:25 -0700

    obtained list of 17 SDGs via API

---
+++ api-explore.py
@@ -0,0 +1,25 @@
+# exploring the UN's SDG API
+
+import requests
+
+# 1. Getting a list of all 17 goals
+
+goal_list = requests.get('https://unstats.un.org/SDGAPI/v1/sdg/Goal/List?includechildren=false')
+
+goals = goal_list.json()
+
+for goal in goals:
+    code = goal['code']
+    name = goal['title']
+    descr = goal['description']
+    uri = goal['uri']
+    print(f'{code}. {name}: \n {descr} \n {uri}')
+
+# payload = {'goal': 'years'}
+
+# res = requests.post('https://unstats.un.org/SDGAPI/v1/sdg/DataAvailability/GetWorldbyGoal', data=payload)
+# print(res.text)
+
+# I think the right link is: https://unstats.un.org/SDGAPI/v1/sdg/
+# goal_targets = res.json()
+
