f585d830d46bdd218bea908d6cd482b9071d8a50
Taiyo Scanlon-Kimura <taiyo.scanlon-kimura@fulbrightmail.org>
2023-05-11 11:47:34 -0700

    implemented progress bar loaded with each sdg's progress

--- static/js/charts.js
+++ static/js/charts.js
@@ -1,37 +1,21 @@
 'use strict';
 
-// 1. Testing a chart
 
-// new Chart(document.querySelector('#progress-chart'), {
-//     type: 'bar',
-//     data: {
-//         labels: [''],
-//         datasets: [
-//             {
-//                 label: 'progress',
-//                 data: [50],
-//             },
-//         ],
-//     },
-//     options: {
-//         indexAxis: 'y',
-//     }
-// });
+// AJAX fetch for SDG progress data
 
-// 2. AJAX fetch for SDG progress data
 const goalcode = document.querySelector('#goalcode').innerHTML;
-console.log(goalcode);
 
 fetch(`/progress_data.json/${goalcode}`)
 .then(response => response.json())
 .then(responseJson => {
-    console.log(responseJson)
     const progress_data = {
         progress: responseJson.progress,
         years_from_start: responseJson.ytd
     };
     
-    new Chart(document.querySelector('#progress-chart'), {
+    // progress bar displaying the progress data
+
+    new Chart(document.querySelector('#progress-bar'), {
         type: 'bar',
         data: {
             labels: [''],
@@ -44,24 +28,24 @@ fetch(`/progress_data.json/${goalcode}`)
         },
         options: {
             indexAxis: 'y',
+            scales: {
+                x: {
+                    max: 100,
+                    ticks: {
+                        callback: value => `${value}%`
+                    }
+                }
+            },
+            plugins: {
+                legend: {
+                    display: false
+                },
+                tooltip: {
+                    enabled: false
+                }
+            }
         }
     });
-
-    // new Chart(document.querySelector('#progress-chart-ajax'), {
-    //     type: 'bar',
-    //     data: {
-    //         labels: [''],
-    //         datasets: [
-    //             {
-    //                 label: 'YTD',
-    //                 data: [15],
-    //             },
-    //         ],
-    //     },
-    //     options: {
-    //         indexAxis: 'y',
-    //     }
-    // });
 });
 
 // 3. Creating a progress chart using progress data
