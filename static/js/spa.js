cf1df8e9bbe9910340f0b82d793ad073df1fc227
Taiyo Scanlon-Kimura <taiyo.scanlon-kimura@fulbrightmail.org>
2023-05-23 09:31:22 -0700

    successful test of event management in sidebar on goals list page; successful ajax fetch for each goal's progress and years to date

--- static/js/spa.js
+++ static/js/spa.js
@@ -1,57 +1,53 @@
 'use strict';
 
-// test
+// create event listeners for all goal buttons
 
-const inputTests = document.querySelectorAll('.test-in');
-const outputTests = document.querySelectorAll('.test-out');
+// const inputTests = document.querySelectorAll('button.test-in');
+// const outputTest = document.querySelector('.test-out');
 
-console.log(inputTests);
-console.log(outputTests);
-
-for (const inputTest of inputTests) {
-    inputTest.addEventListener('click', (evt) => {
-        for (const outputTest of outputTests) {
-            // let testButton = evt.target;
-            outputTest.insertAdjacentHTML('beforeend', 'help me');
-        }
-    }); 
-}
+// for (const inputTest of inputTests) {
+//     inputTest.addEventListener('click', () => {
+//         outputTest.innerHTML = 'this works three';
+//     }); 
+// }
 
 
 // // event handler (SPA)
 
 // // 1. create function to display results for a sdg
 
-// function displayDash(results) {
-//     const goalProgress = results.progress;
-//     const goalYtd = results.ytd;
+function displayDash(results) {
+    const goalProgress = results.progress;
+    const goalYtd = results.ytd;
 
-//     const goalProgressEle = document.querySelector('#dashboard-progress');
-//     const goalYtdEle = document.querySelector('#dashboard-ytd');
+    const goalProgressEle = document.querySelector('#dashboard-progress');
+    const goalYtdEle = document.querySelector('#dashboard-ytd');
 
-//     goalProgressEle.innerHTML = null;
-//     goalYtdEle.innerHTML = null;
+    goalProgressEle.innerHTML = null;
+    goalYtdEle.innerHTML = null;
 
-//     goalProgressEle.innerText = goalProgress;
-//     goalYtdEle.innerText = goalYtd;
-// }
+    goalProgressEle.innerText = goalProgress;
+    goalYtdEle.innerText = goalYtd;
+}
 
 // // 2. create event handler for each goal's button
-// const inputGoals = document.querySelectorAll('.btn-goal');
-// // console.log(inputGoals);
+const inputGoals = document.querySelectorAll('#spa-goal-code');
 
-// for (const inputGoal of inputGoals) {
+for (const inputGoal of inputGoals) {
 
-//     inputGoal.addEventListener('click', (evt) => {
-//         evt.preventDefault();
+    // console.log(inputGoal.innerText);
+
+    inputGoal.addEventListener('click', (evt) => {
+        evt.preventDefault();
+
+        const inputGoalEle = inputGoal.querySelector('.spa-test-in');
+        console.log(inputGoalEle);
+        const goalcode = Number(inputGoalEle.innerText);
         
-//         const goalcode = Number(inputGoal);
-        
-//         fetch(`/progress_data.json/${goalcode}`)
-//         .then(response => response.json())
-//         .then(responseJson => {
-//             displayDash(responseJson);
-//         });
-//         console.log(responseJson);
-//     });
-// }
+        fetch(`/progress_data.json/${goalcode}`)
+        .then(response => response.json())
+        .then(responseJson => {
+            displayDash(responseJson);
+        });
+    });
+}
